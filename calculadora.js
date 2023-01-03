a1bbef3a12418a9f00bce8f40a9631759b842d8b
wil098 <76451123+wil098@users.noreply.github.com>
2023-01-03 16:25:18 -0600

    update 1

---
+++ calculadora.js
@@ -0,0 +1,27 @@
+const sheets = document.querySelector('#sheets');
+const prueba = document.querySelector('#prueba')
+const paper = document.querySelector('#paper');
+const copies = document.querySelector('#copies')
+const total = document.querySelector('#total')
+const bton = document.querySelector('#btnCalcular')
+const priceQuimPaper = 0.06
+const priceBondPaper = 0.05
+
+
+
+    function btnOnClick() {
+        if (paper.value == 'quimico'){
+            const sumaDatos = sheets.value * copies.value * priceQuimPaper;
+        total.innerText = sumaDatos;
+
+        } else if (paper.value == 'bond2'){
+            const sumaDatos = sheets.value * copies.value * priceBondPaper;
+        total.innerText = sumaDatos;
+        }else {
+            const sumaDatos = sheets.value * priceBondPaper * 1 ;
+        total.innerText = sumaDatos;
+        }
+        
+    }
+
+
